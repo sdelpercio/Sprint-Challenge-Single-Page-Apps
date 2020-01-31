6b4f7a4eb862a30f33cd628c920d455d9e559da6
Shawn DelPercio <sdelperc@gmail.com>
2020-01-31 11:14:53 -0500

    created initial state and useeffect for get request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import Header from "./components/Header.js";
 
 
 export default function App() {
+  // setting up state for rickandmorty api
+  const [characterData, setCharacterData] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then((res) => {
+      console.log('response here', res);
+      setCharacterData(res.data.results);
+    })
+    .catch((err) => {
+      console.log('hey, an error', err);
+    })
+  }, [])
+
   return (
     <main>
       <Header />
