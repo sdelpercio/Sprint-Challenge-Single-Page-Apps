e8cfe5359c2a8e4cafab2aaa0237f6d863822fec
Shawn DelPercio <sdelperc@gmail.com>
2020-01-31 11:26:13 -0500

    added routes for welcome and character pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
+import {Route} from 'react-router-dom';
 import axios from 'axios';
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+import SearchForm from './components/SearchForm';
 
 
 export default function App() {
-  // setting up state for rickandmorty api
+  // setting up state for rickandmorty api, then get request
   const [characterData, setCharacterData] = useState([]);
-
   useEffect(() => {
     axios
     .get('https://rickandmortyapi.com/api/character/')
@@ -22,6 +25,13 @@ export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path='/'>
+        <WelcomePage />
+      </Route>
+      <Route path='/characters' >
+        <SearchForm />
+        <CharacterList characterData={characterData} />
+      </Route>
     </main>
   );
 }
